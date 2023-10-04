8631669514771f4246b46d76aa2a8ce71c099778
maxel <axel.maerten@ynov.com>
2023-10-04 16:09:54 +0200

    ma foi

---
+++ IsOdd.cs
@@ -0,0 +1,17 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class IsOdd_Exercice
+    {
+        public static bool IsOdd(int a)
+        {
+            if (a % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
