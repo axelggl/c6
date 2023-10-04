a7e6256f51a0418de414aaac6fd21a97a9f7229f
maxel <axel.maerten@ynov.com>
2023-10-04 17:50:30 +0200

    mais c'est facile en fait

---
+++ RemoveSpaces.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class RemoveSpaces_Exercice
+    {
+        public static string RemoveSpaces(string str)
+        {
+           string result =  str.Trim();
+           return result;
+        }
+    }
+}
