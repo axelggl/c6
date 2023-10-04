22013fe6afd3f71187970e5c6145296bd15df3d2
maxel <axel.maerten@ynov.com>
2023-10-04 16:04:58 +0200

    hello world en fait

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            string a = "Hello World !";
+            return a;
+        }
+    }
+}
