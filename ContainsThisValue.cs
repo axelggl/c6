84333cdda523d04bbd78e0422e7f0e687a20d44e
maxel <axel.maerten@ynov.com>
2023-10-05 16:19:30 +0200

    vrai ou faux genre xdddd

--- ContainsThisValue.cs
+++ ContainsThisValue.cs
@@ -5,7 +5,7 @@ namespace CSharpDiscovery.Quest02
         public static bool ContainsThisValue(int[] tab, int value)
         {
 
-           if (value == null)
+           if (tab == null)
            {
             return false;
            }
