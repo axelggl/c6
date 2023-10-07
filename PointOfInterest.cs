ec4a687abe48be03bb74ce7648d2b8a9d37781f3
maxel <axel.maerten@ynov.com>
2023-10-07 13:07:06 +0200

    il a dit c'est abordable

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -8,6 +8,6 @@ namespace CSharpDiscovery.Quest03
         public double Longitude { get; set; }
         public string Name { get; set; }
 
-        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/search/?api=1&query={0},{1}";
+        public static string GoogleMapsUrlTemplate { get; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
