429c857188bacfad519b0382927e4f371d8a9831
maxel <axel.maerten@ynov.com>
2023-10-07 13:47:35 +0200

    les soucis

--- Campus.cs
+++ Campus.cs
@@ -1,22 +1,11 @@
-using System;
+namespace CSharpDiscovery.Quest03;
 
-namespace CSharpDiscovery.Quest03
+public class Campus : PointOfInterest
 {
-    public class Campus : PointOfInterest
+    public int Capacity { get; set; }
+
+    public override string ToString()
     {
-        public int Capacity { get; set; }
-
-        public Campus(string name, double latitude, double longitude, int capacity)
-        {
-            Name = name;
-            Latitude = latitude;
-            Longitude = longitude;
-            Capacity = capacity;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name} is a campus with a capacity of {Capacity} students";
-        }
+        return $"{Name} is a campus with a capacity of {Capacity} students";
     }
 }
