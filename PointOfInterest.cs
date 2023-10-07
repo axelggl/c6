2f367b26e1aab51531f1cfef5f7662cc2255d58e
maxel <axel.maerten@ynov.com>
2023-10-07 13:10:28 +0200

    et montpellier on a le droit ouuuuu

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -9,5 +9,19 @@ namespace CSharpDiscovery.Quest03
         public string Name { get; set; }
 
         public static string GoogleMapsUrlTemplate { get; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+
+        public PointOfInterest()
+        {
+            Name = "Bordeaux Ynov Campus";
+            Latitude = 44.854186;
+            Longitude = -0.5663056;
+        }
+
+        public PointOfInterest(string name, double latitude, double longitude)
+        {
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
     }
 }
