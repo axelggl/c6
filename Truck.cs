dcf5028ca77b7be5d61eb02ed594860459a3b87f
maxel <axel.maerten@ynov.com>
2023-10-08 11:51:35 +0200

    j'oublie toujours de mettre le point virgule

---
+++ Truck.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace CSharpDiscovery.Quest04
+{
+    public class Truck : Vehicule
+    {
+        public int Tonnage { get; set; }
+
+        public Truck() : base()
+        {
+            Tonnage = 0;
+        }
+
+        public Truck(int Tonnage, string Brand, string Color, int CurrentSpeed = 0) : base(Brand, Color, CurrentSpeed)
+        {
+            this.Tonnage = Tonnage;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {Brand} {Tonnage}T Truck";
+        }
+
+        public override void Accelerate(int Speed)
+        {
+            if (CurrentSpeed + Speed <= 100)
+            {
+                CurrentSpeed += Speed;
+            }
+            else
+            {
+                CurrentSpeed = 100;
+            }
+        }
+
+        public override void Brake(int BrakePower)
+        {
+            if (CurrentSpeed >= 0)
+            {
+                CurrentSpeed -= BrakePower;
+            }
+            else
+            {
+                CurrentSpeed = 0;
+            }
+        }
+
+    }
+}
